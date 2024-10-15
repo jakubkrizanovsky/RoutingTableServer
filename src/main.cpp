6ad85a9172dcfe118251292b183ebd2bf64e453e
Jakub Krizanovsky <jakub.krizanovsky@gmail.com>
2024-10-15 10:37:35 +0200

    Small fix

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 int main() {
     //setup a socket and connection tools
     sockaddr_in serverAddress;
-    bzero((char*)&serverAddress, sizeof(serverAddress));
+    memset((char*)&serverAddress, 0, sizeof(serverAddress));
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
     serverAddress.sin_port = htons(PORT);
